7782d57916d3f4dd0afeda7353b51e01a0a5115a
Tim Mecklem <timothy@mecklem.com>
2019-08-11 22:42:31 -0400

    Add waveshare tft support

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi3.MixProject do
   use Mix.Project
 
-  @app :nerves_system_rpi3
+  @app :nerves_system_rpi3_tft
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
